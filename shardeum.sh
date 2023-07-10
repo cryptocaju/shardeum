380b8e6b810b3e4a4eaf024ddb4fa29821b62e24
cryptocaju <49980519+cryptocaju@users.noreply.github.com>
2023-07-10 15:35:42 +0100

    Update shardeum.sh

---
+++ shardeum.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if exists curl; then
+	echo ''
+else
+  sudo apt install curl -y < "/dev/null"
+fi
+curl -s https://github.com/cryptocaju/shardeum/logocaju.sh | bash
+echo "==================================================="
+sleep 2
+sudo apt update && sudo apt-get install software-properties-common -y
