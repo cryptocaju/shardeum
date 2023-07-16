be7446a9e86d7421ccacb55b79146dc352f4606a
cryptocaju <49980519+cryptocaju@users.noreply.github.com>
2023-07-16 23:11:22 +0100

    Update shardeum.sh

--- shardeum.sh
+++ shardeum.sh
@@ -1,11 +1,38 @@
-#!/bin/bash
+echo -e '\e[40m\e[92m'                          
+echo -e '                                                                                                                   '
+echo -e '       /$$$$$$   /$$$$$$     /$$$$$ /$$   /$$        /$$$$$$  /$$$$$$$  /$$     /$$ /$$$$$$$  /$$$$$$$$  /$$$$$$   '
+echo -e '      /$$__  $$ /$$__  $$   |__  $$| $$  | $$       /$$__  $$| $$__  $$|  $$   /$$/| $$__  $$|__  $$__/ /$$__  $$  '
+echo -e '     | $$  \__/| $$  \ $$      | $$| $$  | $$      | $$  \__/| $$  \ $$ \  $$ /$$/ | $$  \ $$   | $$   | $$  \ $$  '
+echo -e '     | $$      | $$$$$$$$      | $$| $$  | $$      | $$      | $$$$$$$/  \  $$$$/  | $$$$$$$/   | $$   | $$  | $$  '
+echo -e '     | $$      | $$__  $$ /$$  | $$| $$  | $$      | $$      | $$__  $$   \  $$/   | $$____/    | $$   | $$  | $$  '
+echo -e '     | $$    $$| $$  | $$| $$  | $$| $$  | $$      | $$    $$| $$  \ $$    | $$    | $$         | $$   | $$  | $$  '
+echo -e '     |  $$$$$$/| $$  | $$|  $$$$$$/|  $$$$$$/      |  $$$$$$/| $$  | $$    | $$    | $$         | $$   |  $$$$$$/  '
+echo -e '      \______/ |__/  |__/ \______/  \______/        \______/ |__/  |__/    |__/    |__/         |__/    \______/   '
+echo -e '                                                                                                                   '
+echo -e '\e[0m'     
+echo "======================================================================================================================="
+echo "===========================================VAMOS COMECAR================================================================"
 
-if exists curl; then
-	echo ''
-else
-  sudo apt install curl -y < "/dev/null"
-fi
-curl -s https://github.com/cryptocaju/shardeum/logocaju.sh | bash
-echo "==================================================="
-sleep 2
-sudo apt update && sudo apt-get install software-properties-common -y
+
+sudo apt-get install curl
+
+sudo apt update
+
+sudo apt install docker.io
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh && chmod +x installer.sh && ./installer.sh
+
+
+cd
+
+cd .shardeum
+
+./shell.sh
+
+operator-cli gui start
+
+
