17c349e1ac56d1291518f76a058c25bbcab07c8b
cryptocaju <49980519+cryptocaju@users.noreply.github.com>
2023-07-20 00:21:53 +0100

    Update shardeum.sh

--- shardeum.sh
+++ shardeum.sh
@@ -12,16 +12,17 @@ echo -e '                                                                       
 echo -e '\e[0m'  
 
 echo  -e  "\e[1;33m===============================SEJA-BENVINDO =============================================V.001===================\e[0m"
+sleep 8s
+echo  -e  "\e[1;33m===============================INICIANDO EM 5 SEGUNDOS============================================================\e[0m"
 sleep 5s
-echo  -e  "\e[1;33m===============================INICIANDO EM 3 SEGUNDOS============================================================\e[0m"
-sleep 3s
-sudo apt-get install curl
-sudo apt update
-echo  -e  "\e[1;34m================================INSTALANDO DOCKER===============================================================\e[0m"
+sudo apt update && upgrade -y
+echo  -e  "\e[1;33m================================INSTALANDO DOCKER===============================================================\e[0m"
 sleep 7s
 sudo apt-get install -y docker.io
-echo  -e  "\e[1;34m================================INSTALACAO COM SUCESSO==========================================================\e[0m"
+echo  -e  "\e[1;33m================================INSTALACAO COM SUCESSO==========================================================\e[0m"
 docker --version
+sudo apt-get install curl -y
 echo  -e  "\e[1;34m================================INSTALACAO DOCKER COMPOSE=======================================================\e[0m"
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
+echo  -e  "\e[1;33m============================INSTALACAO COMPLETA, VAMOS AO PROXIMO PASSO :)=======================================\e[0m"
