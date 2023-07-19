d0d287dd3b786110f9d8272d5c1342b34e84e134
cryptocaju <49980519+cryptocaju@users.noreply.github.com>
2023-07-19 14:22:40 +0100

    Update shardeum.sh

--- shardeum.sh
+++ shardeum.sh
@@ -12,5 +12,5 @@ echo -e '                                                                       
 
 sudo apt-get install curl
 sudo apt update
-sudo apt install -y docker.io
+apt-get install -y docker.io
 docker --version
